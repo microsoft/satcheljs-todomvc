4ef31c22416997d35f11301f8e8a747915edea0e
Scott Mikula <mikula@gmail.com>
2017-03-08 08:26:41 -0800

    Convert App to a stateless functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,12 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
-export default class App extends React.Component<any, any> {
-    render() {     
-        return (
-            <div>
-                <Header />                
-                <Main />
-                <Footer />
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
 }
