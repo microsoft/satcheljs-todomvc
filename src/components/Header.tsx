e3cdc55b68e38b5abe1aa6fd9d573def75af129c
Scott Mikula <mikula@gmail.com>
2017-03-08 09:03:33 -0800

    Cleanup Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,35 @@
 import * as React from 'react';
 import {observer} from 'mobx-react';
-import Store from '../store/store';
+import store from '../store/store';
 import addItem from '../actions/addItem';
 import updateTextboxValue from '../actions/updateTextboxValue';
 
 @observer
-export default class Header extends React.Component<any, any> {
+export default class Header extends React.Component<{}, {}> {
     private textbox: HTMLInputElement;
 
     onKeyPress = (e) => {
         if (e.key == "Enter") {
             addItem(this.textbox.value);
         }
+
         return true;
     }
 
     onChange = () => {
-        // for ephemeral UI state changes, it is okay to use a function
+        // Update the store every time the textbox value changes.
         updateTextboxValue(this.textbox.value);
     }
 
     render() {
-        let {textboxValue} = Store;
-
         return (
             <header className="header">
                 <h1>todos</h1>
                 <input
-                    ref={ctrl => this.textbox = ctrl}
+                    ref={ref => this.textbox = ref}
                     className="new-todo"
                     placeholder="What needs to be done?"
-                    value={textboxValue}
+                    value={store.textboxValue}
                     autoFocus={true}
                     onChange={this.onChange}
                     onKeyPress={this.onKeyPress} />
