cd865b48842a4a9e70c609a9e15a567e4743a7dc
Scott Mikula <mikula@gmail.com>
2017-03-15 17:36:27 -0700

    Convert Header to be a stateless functional component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import NewTaskInput from './NewTaskInput';
 
-export default class Header extends React.Component<{}, {}> {
-    render() {
-        return (
-            <header className="header">
-                <h1>todos</h1>
-                <NewTaskInput />
-            </header>
-        )
-    }
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>todos</h1>
+            <NewTaskInput />
+        </header>
+    );
 }
