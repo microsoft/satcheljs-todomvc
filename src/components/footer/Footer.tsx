297f8a74b8eceab317e7071fa858e52ac8d99f99
Scott Mikula <mikula@gmail.com>
2017-03-15 17:57:52 -0700

    Factor FilterButton out of Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,6 +5,7 @@ import removeAllCompleted from '../../actions/removeAllCompleted';
 import setFilter from '../../actions/setFilter';
 import {FilterType} from '../../store/schema';
 import ItemsLeftCount from './ItemsLeftCount';
+import FilterButton from './FilterButton';
 
 @observer
 export default class Footer extends React.Component<any, any> {
@@ -12,20 +13,14 @@ export default class Footer extends React.Component<any, any> {
         removeAllCompleted();
     }
 
-    onSetFilter = (filter: FilterType) => {
-        setFilter(filter);
-    }
-
     render() {
-        let {filter} = Store;
-
         return (
             <footer className="footer">
                 <ItemsLeftCount />
                 <ul className="filters">
-                    <li><a href="#" onClick={() => this.onSetFilter(FilterType.All)} className={filter == FilterType.All ? "selected" : ""}>All</a></li>
-                    <li><a href="#" onClick={() => this.onSetFilter(FilterType.Active)} className={filter == FilterType.Active ? "selected" : ""}>Active</a></li>
-                    <li><a href="#" onClick={() => this.onSetFilter(FilterType.Completed)} className={filter == FilterType.Completed ? "selected" : ""}>Completed</a></li>
+                    <FilterButton filter={FilterType.All} text="All" />
+                    <FilterButton filter={FilterType.Active} text="Active" />
+                    <FilterButton filter={FilterType.Completed} text="Completed" />
                 </ul>
                 <button className="clear-completed" onClick={this.onClearCompleted}>Clear completed</button>
             </footer>
