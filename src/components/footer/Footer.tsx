be147dd32ac8f3e138b726377a7caf2225638c86
Scott Mikula <mikula@gmail.com>
2017-03-16 08:42:55 -0700

    Convert Footer to a stateless functional component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,19 @@
 import * as React from 'react';
-import {observer} from 'mobx-react';
 import removeAllCompleted from '../../actions/removeAllCompleted';
-import {FilterType} from '../../store/schema';
+import { FilterType } from '../../store/schema';
 import ItemsLeftCount from './ItemsLeftCount';
 import FilterButton from './FilterButton';
 
-@observer
-export default class Footer extends React.Component<any, any> {
-    onClearCompleted = () => {
-        removeAllCompleted();
-    }
-
-    render() {
-        return (
-            <footer className="footer">
-                <ItemsLeftCount />
-                <ul className="filters">
-                    <FilterButton filter={FilterType.All} text="All" />
-                    <FilterButton filter={FilterType.Active} text="Active" />
-                    <FilterButton filter={FilterType.Completed} text="Completed" />
-                </ul>
-                <button className="clear-completed" onClick={this.onClearCompleted}>Clear completed</button>
-            </footer>
-        );
-    }
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <ItemsLeftCount />
+            <ul className="filters">
+                <FilterButton filter={FilterType.All} text="All" />
+                <FilterButton filter={FilterType.Active} text="Active" />
+                <FilterButton filter={FilterType.Completed} text="Completed" />
+            </ul>
+            <button className="clear-completed" onClick={removeAllCompleted}>Clear completed</button>
+        </footer>
+    );
 }
